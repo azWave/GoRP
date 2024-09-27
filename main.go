beb730d3af3704983beaf1ea23b0d6415546535b
azWave <bourre.maxime.62@gmail.com>
2024-09-27 18:04:37 +0200

    rajout des classes players & roles + ajout des Roles par defaults

--- main.go
+++ main.go
@@ -6,26 +6,41 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Roles struct {
+	Name string `json:"name"`
+	Health int `json:"health"`
+	Mana int `json:"mana"`
+	Stength int `json:"strength"`
+	Armor int `json:"armor"`
+}
 
-type user struct {
-	Id	int `json:"id"`
+type player struct {
 	Username string `json:"username"`
+	Role Roles `json:"role"`
+	Health int `json:"health"`
+	Mana int `json:"mana"`
+	Stength int `json:"strength"`
+	Armor int `json:"armor"`	
 }
 
-var users = []user{
-	{Id:123 , Username :"Simon"},
-	{Id:124 , Username :"Maxime"},
+
+
+
+var roles = []Roles{
+	{Name : "Warrior",Health : 150,Mana : 50,Stength : 200,Armor:150},
+	{Name : "Thief",Health : 100,Mana : 75,Stength : 100,Armor:75},
+	{Name : "Mage",Health : 120,Mana : 200,Stength : 75,Armor:100},
 }
 
 
 
-func getUsers(c *gin.Context){
-	c.IndentedJSON(http.StatusOK, users)
+func getRoles(ctx *gin.Context){
+	ctx.IndentedJSON(http.StatusOK, roles)
 }
 
 func main () {
 	router := gin.Default()
-	router.GET("/users",getUsers)
+	router.GET("/roles",getRoles)
 
 	router.Run("localhost:8080")
 
