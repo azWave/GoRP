ba67ec05fcea3cdcb0896e4ba895331ae86beed3
azWave <bourre.maxime.62@gmail.com>
2024-09-27 17:45:56 +0200

    fin du tuto

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
+
+type user struct {
+	Id	int `json:"id"`
+	Username string `json:"username"`
+}
+
+var users = []user{
+	{Id:123 , Username :"Simon"},
+	{Id:124 , Username :"Maxime"},
+}
+
+
+
+func getUsers(c *gin.Context){
+	c.IndentedJSON(http.StatusOK, users)
+}
+
 func main () {
 	router := gin.Default()
+	router.GET("/users",getUsers)
+
 	router.Run("localhost:8080")
+
 	
 }
