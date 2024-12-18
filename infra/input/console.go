b243fdc18bec4562da3f04e8584d5b3e7b7598e8
Sygix <17769298+Sygix@users.noreply.github.com>
2024-12-18 17:32:01 +0100

    feat(console): add printing character summary

--- infra/input/console.go
+++ infra/input/console.go
@@ -2,23 +2,61 @@ package input
 
 import (
 	"fmt"
+	"gorp/core/domain/entities"
 	"gorp/core/services"
 )
 
-func CharacterCreationHandler(service *services.CharacterService) {
+func CharacterCreationHandler(service *services.CharacterService) entities.Character {
 	var name, class string
 
+	fmt.Println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+	fmt.Println("Création d'un personnage")
+	fmt.Println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
 	fmt.Println("Entrez le nom du personnage :")
 	fmt.Scanln(&name)
 
-	fmt.Println("Choisissez une classe (Guerrier, Mage, Voleur) :")
+	fmt.Println("\nChoisissez une classe (Guerrier, Mage, Voleur) :")
 	fmt.Scanln(&class)
 
 	character, err := service.CreateCharacter(name, class)
 	if err != nil {
 		fmt.Println("Erreur :", err)
-		return
+		return entities.Character{}
 	}
 
-	fmt.Printf("Personnage créé avec succès :\n%+v\n", character)
+	fmt.Println("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+	fmt.Println("Personnage créé avec succès.")
+	fmt.Println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+	PrintCharacterSummary(character)
+
+	return character
+}
+
+func PrintCharacterSummary(character entities.Character) {
+	fmt.Printf(
+		"Nom : %v\n"+
+			"Classe : %v\n"+
+			"PV : %v\n"+
+			"PM : %v\n"+
+			"Force : %v\n"+
+			"Intelligence : %v\n"+
+			"Défense : %v\n"+
+			"Résistance Magique : %v\n"+
+			"Agilité : %v\n"+
+			"Chance : %v\n"+
+			"Endurance : %v\n"+
+			"Esprit : %v\n",
+		character.Name,
+		character.Class,
+		character.Stats.Health,
+		character.Stats.Mana,
+		character.Stats.Strength,
+		character.Stats.Intelligence,
+		character.Stats.Defense,
+		character.Stats.MagicResist,
+		character.Stats.Agility,
+		character.Stats.Luck,
+		character.Stats.Endurance,
+		character.Stats.Spirit,
+	)
 }
